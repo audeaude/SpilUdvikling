231c67bc28e9c479541b57971960c22f9b83e9dd
LAPTOP-M80BD5VA\mathi <mathiasaude@gmail.com>
2016-10-26 00:24:04 +0200

    no message

---
+++ BikerQuest/Assets/Scripts/SS_Script.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SS_Script : MonoBehaviour {
+	public bool mouseOverSS = false; 
+	private float blinkTrig = 0; 
+	Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+		
+	void OnMouseEnter(){ 
+		mouseOverSS = true;
+		animator.SetTrigger ("PointerEnter");
+	}
+
+	void OnMouseExit(){ 
+		mouseOverSS = false;
+		animator.SetTrigger ("PointerExit");
+	}
+
+
+	void Update(){
+	
+		blinkTrig = Random.value; 
+
+		if (blinkTrig >= 0.98) {
+			animator.SetTrigger ("BlinkTrigger");
+		} else {}
+	}
+}
+
